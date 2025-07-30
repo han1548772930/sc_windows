59767af5a8ee4249623cd7303a1ed741e9640f26
hl <hl@nmcsoft.com>
2025-07-30 14:56:18 +0800

    增加翻译模块

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,12 @@ pub mod file_dialog;
 pub mod input;
 pub mod ocr; // OCR 文本识别模块
 pub mod ocr_result_window; // OCR 结果显示窗口
+
 pub mod simple_settings; // 原生 Windows API 设置窗口
 pub mod svg_icons;
 pub mod system_tray;
 pub mod toolbar;
+pub mod translation; // 翻译模块
 pub mod types;
 pub mod utils;
 pub mod window_detection; // 窗口检测模块
